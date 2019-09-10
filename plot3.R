2a4271e921df490c70be5e229b76a08bf7ed2f7b
ChannonSam <samuel.channon@cantab.net>
2019-09-10 07:57:21 +0100

    Forgot to add legend to plot 3!

--- plot3.R
+++ plot3.R
@@ -11,4 +11,6 @@ with(data3, plot(x = date_time, y = Sub_metering_1, pch = NA,
 with(data3, lines(x = date_time, y = Sub_metering_1, col = "black"))
 with(data3, lines(x = date_time, y = Sub_metering_2, col = "red"))
 with(data3, lines(x = date_time, y = Sub_metering_3, col = "blue"))
+legend("topright", col = c("black", "red", "blue"),
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 dev.off()
